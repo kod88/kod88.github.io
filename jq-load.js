751dcdb4331e6665d2d0b99a9d962b41ece34638
kod88 <36810090+kod88@users.noreply.github.com>
2018-06-17 14:52:00 +0300

    Delete jq-load.js

--- jq-load.js
+++
@@ -1,9 +0,0 @@
-﻿//AJAX HTML
-$('nav a').on('click', function(e) {          
-  e.preventDefault();
-  var url = this.href;
-  $('nav a.current').removeClass('current');
-  $(this).addClass('current');
-  $('#container').remove();
-  $('#content').load(url + ' #container').hide().fadeIn('slow');
-});
