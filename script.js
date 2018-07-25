4bf864b73ac58e7701aa7263223183cdc18fd388
Konstantin <nokia300198@mail.ru>
2018-07-25 15:59:42 +0300

    1
    1

---
+++ script.js
@@ -0,0 +1,10 @@
+var 
+		content = document.getElementById("content"),
+		canvas = document.getElementById("canvas"),
+		innerHeight = window.innerHeight ;
+
+var contentHeight = (innerHeight) + "px";
+		content.style.height=contentHeight;
+
+var canvasHeight = (innerHeight * (3/4)) + "px";
+		canvas.style.height=canvasHeight
